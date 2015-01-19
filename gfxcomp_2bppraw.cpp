7fe4c364477e6d6932e3a094ff3e25d5ddbf6ae4
Maxim <maxim@smspower.org>
2015-01-19 19:04:52 +0000

    Added 2bpp, 3bpp "compressors"

---
+++ gfxcomp_2bppraw.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <cstdint>
+
+extern "C" __declspec(dllexport) const char* getName()
+{
+	// A pretty name for this compression type
+	// Generally, the name of the game it was REd from
+	return "2bpp raw (uncompressed) binary";
+}
+
+extern "C" __declspec(dllexport) const char* getExt()
+{
+	// A string suitable for use as a file extension
+	return "2bpp";
+}
+
+extern "C" __declspec(dllexport) uint32_t compressTiles(uint8_t* source, uint32_t numTiles, uint8_t* dest, uint32_t destLen)
+{
+	uint32_t sourceLen = numTiles * 32;
+	if (sourceLen / 2 > destLen)
+	{
+		return 0;
+	}
+	else
+	{
+		for (uint32_t i = 0; i < sourceLen; ++i)
+		{
+			*dest++ = *source;
+			*dest++ = *source;
+			source += 2;
+		}
+		return sourceLen / 2;
+	}
+}
