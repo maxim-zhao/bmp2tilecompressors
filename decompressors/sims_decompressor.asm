8b5a73fe97b58303bbdd1a1061eac633f0a12d75
Maxim <maxim@smspower.org>
2024-12-10 21:12:52 +0000

    Better commented SIMS decompressor

--- decompressors/sims_decompressor.asm
+++ decompressors/sims_decompressor.asm
@@ -1,238 +1,237 @@
-
-;-----------------------------------------------------------------
-;           Master of Darknesss - Decompression Routine
-;-----------------------------------------------------------------
+; Format:
+; %0nnnnnnn %NNNNcccc   LZ copy: distance nN (0..2047) count c+2 (2..17)
+; %10nnnnnn $dd...      Raw copy: n+1 bytes (1..64)
+; %110llnnn $dd...      Short semi-RLE: n+2 repetitions (2..9) of length l+1 (1..4)
+; %111llnnn $NN $dd...  Long semi-RLE: nN+2 repetitions (2..2047) of length l+1 (1..4)
 
 .enum SIMSDecompressorMemory
   LZ_WINDOW            dsb 2048 ; Lookback for LZ
-  LZ_WINDOW_CURSOR     dw ; Pointer into it
-  COMPRESSED_DATA_SIZE dw ; Pointer to end of input data
+  LZ_WINDOW_CURSOR     dw ; Index into it
+  COMPRESSED_DATA_END  dw ; Pointer to end of input data
   LZ_COPY_LENGTH       db ; Another counter
   LZ_COPY_TEMP_BUFFER  dsb 17 ; LZ runs are put there before being appended to the window
 .ende
 
 SIMSDecompress:
-; Parameters:
-; de = VRAM address, with write bit set
-; hl = pointer to data
-        ; Initialise window
-        ex af, af'
-        exx
-          xor a
-          ld hl, LZ_WINDOW
-          ld bc, $0820
-          dec bc
-          ld (hl), a
-          ld d, h
-          ld e, l
-          inc de
-          ldir
-        exx
-        ex af, af'
+  ; Parameters:
+  ; de = VRAM address, with write bit set
+  ; hl = pointer to data
 
-        ld c, $bf
-        out (c), e
-        out (c), d
-        ; Read DATA SIZE, first 2 bytes from COMPRESSED DATA
-        ld c, (hl)
-        inc hl
-        ld b, (hl)
-        inc hl
-        add hl, bc
-        ld (COMPRESSED_DATA_SIZE), hl
-        or a
-        sbc hl, bc
+  ; Initialise window to zeros
+  exx
+    xor a
+    ld hl, LZ_WINDOW
+    ld bc, $0820 ; 2080 = all the memory and then some
+    dec bc
+    ld (hl), a
+    ld d, h
+    ld e, l
+    inc de
+    ldir
+  exx
+
+  ; Set VRAM address
+  ld c, $bf
+  out (c), e
+  out (c), d
+  ; Read the data size
+  ld c, (hl)
+  inc hl
+  ld b, (hl)
+  inc hl
+  ; Compute the end address
+  add hl, bc
+  ld (COMPRESSED_DATA_END), hl
+  ; Restore HL
+  or a
+  sbc hl, bc
 
 DECOMPRESSOR_LOOP:
-        ld bc, (COMPRESSED_DATA_SIZE)
-        or a
-        sbc hl, bc
-        ret nc
-        add hl, bc
-        call _CHECK_DECOMPRESSION_TYPE
-        jp DECOMPRESSOR_LOOP
+  ; Check if we got to the end
+  ld bc, (COMPRESSED_DATA_END)
+  or a
+  sbc hl, bc
+  ret nc
+  add hl, bc
+  ; If not, do a chunk
+  call _CHECK_DECOMPRESSION_TYPE
+  jp DECOMPRESSOR_LOOP
 
 
-; CHECK DECOMPRESSION TYPE
-; BIT 7 = LZ
-; BIT 6 = RAW
-; BIT 5 = RLE COPY
-; NO BIT = RLE COPY DEFAULT REPETIONS
 _CHECK_DECOMPRESSION_TYPE:
-        ld a, (hl)                 ; Load byte from HL (CURSOR) in A
-        inc hl                     ; Increment HL (CURSOR)
+  ; Read the next byte
+  ld a, (hl)
+  inc hl
+  ; Dispatch based on the upper bits
+  bit 7, a
+  jp z, _LZ           ; %0------- = LZ
+  bit 6, a
+  jp z, _RAW          ; %10------ = raw
+  push af
+    bit 5, a
+    jr z, _RLE_SHORT  ; %110----- = few repeats
 
-        bit 7, a                   ; Test 7th bit of A
-        jp z, _LZ_COPY             ; If set do LZ COPY
-        bit 6, a                   ; Test 6th bit of A
-        jp z, _RAW_COPY            ; If set do RAW COPY
-        push af                    
-        bit 5, a                   ; Test 5th bit of A
-        jr z, _RLE_COPY            ; If set do RLE COPY
+_RLE_LONG:
+    ; Else it is      ; %111----- = many repeats
+    ; Mask to the high distance bits, put them in b
+    and %111
+    ld b, a
+    ; Next byte is additional distance bits, put them in c
+    ld a, (hl)
+    inc hl
+    ld c, a
+    
+    ; Then jump into the RLE copy code
+    jp +
 
-                                   ; GET RLE REPETITIONS
-                                   ; -------------------------------
-                                   ; If all bit test fails
-        and $07                    ; Get first 3 bits of Byte
-        ld b, a
-        ld a, (hl)                 ; Load next byte from HL (CURSOR) in A
-        inc hl                     ; Increment HL (CURSOR)
-        ld c, a
+_RLE_SHORT:
+    ; a is the distance and repeat length
+    ; Mask to the distance bits, put them in bc
+    and %111
+    ld c, a
+    ld b, 0
+    ; fall through
++:  ; bc += 2
+    inc bc
+    inc bc
+  pop af ; restore command byte
+  
+  ; Repeat length is in bits 3-4, minus 1
+  .repeat 3
+    rra
+  .endr
+  and %11
+  inc a
+  ld e, a
+  
+  ; Now do the copy
+--:
+  push bc ; repeat count
+  push hl ; data pointer
+    ld d, e ; run length
+-:  ; Copy a byte from (hl++)
+    ld a, (hl)
+    out ($be), a
+    call _STORE_IN_LZ_WINDOW ; also increments hl
+    dec d
+    jr nz, - ; repeat for the run length (e)
+  pop hl
+  pop bc
+  dec bc ; repeat for the required count of repeats
+  ld a, b
+  or c
+  jr nz, --
+  ; Finally, move hl on by the run length
+  ld d, 0
+  add hl, de
+  ret
 
-        jp _RLE_COPY_GET_LENGTH    ; Skip default GET RLE REPETITIONS
-                                   ; and GET RLE LENGTH
+_LZ:
+  ; Distance = low 7 bits in a followed by high 4 bits in (hl)
+  ; Compute that in bc
+  push hl
+    ld l, a
+    ld h, 0
+    .repeat 4
+      add hl, hl
+    .endr
+    ld b, h
+    ld c, l
+  pop hl
+  ; OR high 4 bits of next byte into c
+  ld a, (hl)
+  .repeat 4
+    rra
+  .endr
+  and $f
+  or c
+  ld c, a
+  
+  ; Now subtract that from LZ_WINDOW_CURSOR, wrapping at 2K so the buffer acts circularly.
+  ; This is still an offset relative to the start of LZ_WINDOW.
+  push hl
+    ld hl, (LZ_WINDOW_CURSOR)
+    or a
+    sbc hl, bc
+    ld a, h
+    and $07
+    ld b, a
+    ld c, l
+  pop hl
 
-; Performs RLE Copy
-; BC  = REPETITIONS
-; E  = LENGTH
-_RLE_COPY:
+  ; Re-read byte and get the length (minus 2) in the low 4 bits
+  ld a, (hl)
+  inc hl
+  and $0F
+  add a, 2
+  ld (LZ_COPY_LENGTH), a
 
-                                    ; GET RLE REPETITIONS
-                                    ; -------------------------------
-        and $07                     ; Get first 3 bits of Byte
-        ld c, a
-        ld b, $00
-_RLE_COPY_GET_LENGTH:
-        inc bc                      ; Increment REPETITIONS
-        inc bc                      ; Increment REPETITIONS
-        pop af
-
-                                    ; GET RLE LENGTH
-                                    ; -------------------------------
-        rra                         ; Bitshift a >> 3
-        rra
-        rra
-        and $03                     ; Get first 2 bits of byte
-        inc a                       ; Increment LENGTH
-        ld e, a
---:                                 ;                          <<<<<<<<<<<<|
-        push bc                     ;                                      |
-        push hl                     ;                                      |
-        ld d, e                     ;                                      |
-                                    ;                                      |
-                                    ;                                      |
--:                                  ;                            <<<<<<<<| |
-        ld a, (hl)                  ; Load Byte from CURSOR (HL) in A    | |
-        out ($be), a                ; Copy Byte to VRAM                  | |
-        call _STORE_IN_LZ_WINDOW    ; Copy Byte do LZ_WINDOW             | |
-        dec d                       ; Decrement D (LENGTH)               | |
-        jr nz, -                    ; If NOT ZERO loop again     >>>>>>>>| |
-                                    ;                                      |
-        pop hl                      ;                                      |
-        pop bc                      ;                                      |
-        dec bc                      ; Decrement BC (REPETITIONS)           |
-        ld a, b                     ;                                      |
-        or c                        ;                                      |
-        jr nz, --                   ; If NOT ZERO loop again   >>>>>>>>>>>>|
-        ld d, $00
-        add hl, de                  ; Add DE (LENGTH) to HL (CURSOR POSITION)
-        ret
-
-; PERFORMS LZ COPY
-_LZ_COPY:
-        push hl
-
-                                    ; GET DISTANCE FROM LZ PAIR
-                                    ; -------------------------------
-        ld l, a
-        ld h, $00
-        add hl, hl                  ; Bitshift HL << 4
-        add hl, hl
-        add hl, hl
-        add hl, hl
-        ld b, h
-        ld c, l
-
-        pop hl
-
-                                    ; GET DISTANCE IN LZ PAIR
-                                    ; -------------------------------
-        ld a, (hl)
-        rra                         ; Bitshift A >> 4
-        rra
-        rra
-        rra
-
-        and $0F                     ; Get first 4 bits from Byte
-        or c                        ; or C
-        ld c, a
-        push hl
-        ld hl, (LZ_WINDOW_CURSOR)   ; Store LZ_WINDOW CURSOR position in HL
-        or a                        ; or A
-        sbc hl, bc                  ; Subtract BC (LZ_WINDOW CURSOR) from HL (DISTANCE)
-        ld a, h
-        and $07                     ; Get first 3 bits of A
-        ld b, a
-        ld c, l
-        pop hl
-                                    ; GET LENGTH IN LZ PAIR
-                                    ; -------------------------------
-        ld a, (hl)                  ; Load next byte from CURSOR (HL) in A
-        inc hl                      ; Increment (HL) CURSOR
-        and $0F                     ; Get first 4 bits of A (LENGTH)
-        add a, $02                  ; Add 2 to A
-        ld (LZ_COPY_LENGTH), a      ; Store A in LZ_COPY_LENGTH
-        push hl
-        push af
-        push de
+  push hl ; preserve read address
+    push af ; preserve LZ run length
+      push de
+        ; We 
         ld de, LZ_COPY_TEMP_BUFFER
--:                                  ;                           <<<<<<<<<<<<|
-        ld hl, LZ_WINDOW            ; Set HL (CURSOR) as LZ_WINDOW          |
-        add hl, bc                  ; Add DISTANCE in HL                    |
-        ld a, (hl)                  ; Load Byte from HL (CURSOR)            |
-        ld (de), a                  ; Copy Byte to Temp Buffer              |
-        out ($be), a       ; Copy Byte to VRAM                     |
-        inc de                      ; Increment Temp Buffer                 |
-        inc bc                      ; Increment DISTANCE                    |
-        ld a, b                     ;                                       |
-        and $07                     ; Get first 3 bits from Byte            |
-        ld b, a                     ;                                       |
-        ld hl, LZ_COPY_LENGTH       ;                                       |
-        dec (hl)                    ; Decrement LENGTH                      |
-        jr nz, -                    ; If NOT ZERO loop again    >>>>>>>>>>>>|
-        pop hl
-        pop af
-        ld hl, LZ_COPY_TEMP_BUFFER  ; Set HL (CURSOR) as Temp Buffer position
-        ld d, a
--:
-                                    ; Copy Temp Buffer to LZ_WINDOW
-                                    ; -------------------------------
-                                    ;                             <<<<<<<<<<<<|
-        call _STORE_IN_LZ_WINDOW    ; Copy Byte to LZ WINDOW                  |
-        dec d                       ; Decrement LENGHT                        |
-        jr nz, -                    ; If NOT ZERO loop again      >>>>>>>>>>>>|
-        pop hl
-        ret
+-:      ; Compute hl = LZ_WINDOW + bc
+        ld hl, LZ_WINDOW
+        add hl, bc
+        ; Copy a byte from there to LZ_COPY_TEMP_BUFFER and to VRAM
+        ld a, (hl)
+        ld (de), a
+        out ($be), a
+        ; Increment dest and index
+        inc de
+        inc bc
+        ; Wrap bc at 2K
+        ld a, b
+        and $07
+        ld b, a
+        ; Decrement the counter
+        ld hl, LZ_COPY_LENGTH
+        dec (hl)
+        ; Loop over the run length
+        jr nz, -
+      pop hl ; Unmatched pop: seemingly doesn't matter? Can be removed?
+    pop af
+    ; Then copy all the bytes from LZ_COPY_TEMP_BUFFER into the window - only after the copy is done
+    ld hl, LZ_COPY_TEMP_BUFFER
+    ld d, a
+-:  call _STORE_IN_LZ_WINDOW ; also increments hl
+    dec d
+    jr nz, - ; djnz would be faster
+  pop hl
+  ret
 
-; PERFORMS RAW COPY
-; D  = LENGTH
-_RAW_COPY:
-                                    ; GET LENGTH
-                                    ; -------------------------------
-        and $3F                     ; Get first 6 bits from Byte
-        inc a                       ; Increment LENGTH
-        ld d, a
+_RAW:
+  ; Low 6 bits of a = length - 1
+  ; Copy bytes from 
+  and $3F
+  inc a
+  ld d, a
+-:; Copy a byte to VRAM
+  ld a, (hl)
+  out ($be), a
+  ; And remember it
+  call _STORE_IN_LZ_WINDOW ; also increments hl
+  dec d ; djnz would be faster
+  jr nz, -
+  ret
 
--:                                  ;                          <<<<<<<<|
-        ld a, (hl)                  ; Load next byte in A              |
-        out ($be), a       ; Copy Byte to VRAM                |
-        call _STORE_IN_LZ_WINDOW    ; Copy ByteA to LZ_WINDOW          |
-        dec d                       ; Decrement D                      |
-        jr nz, -                    ; If NOT ZERO loop again   >>>>>>>>|
-        ret
-
-; STORE BYTE IN LZ WINDOW
 _STORE_IN_LZ_WINDOW:
-        ld a, (hl)                  ; Load byte from CURSOR (HL) in A
-        inc hl                      ; Increment CURSOR (HL)
-        push hl
-        ld hl, LZ_WINDOW            ; Store LZ_WINDOW BASE address in HL
-        ld bc, (LZ_WINDOW_CURSOR)   ; Store LZ_WINDOW CURSOR position in BC
-        add hl, bc                  ; Add BC in HL (BASE address + CURSOR)
-        ld (hl), a                  ; Store Byte in HL (LZ_WINDOW)
-        inc bc                      ; Increment BC (LZ_WINDOW CURSOR position)
-        res 3, b
-        ld (LZ_WINDOW_CURSOR), bc   ; Store LZ_WINDOW CURSOR position
-        pop hl
-        ret
+  ; Copies (hl++) to the "window", wrapping at 2K
+  ; Get byte
+  ld a, (hl)
+  inc hl
+  push hl
+    ; Write to LZ_WINDOW[LZ_WINDOW_CURSOR]
+    ld hl, LZ_WINDOW
+    ld bc, (LZ_WINDOW_CURSOR)
+    add hl, bc
+    ld (hl), a
+    ; Increment and wrap offset
+    inc bc
+    res 3, b
+    ;  And store for next time
+    ld (LZ_WINDOW_CURSOR), bc
+  pop hl
+  ret
         
